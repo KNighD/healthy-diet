d455886b1414bc3525b8cebd99e99b51e3c8e690
knighd <daizhenjie@xiaoyouzi.com>
2020-05-01 16:39:27 +0800

    feat: 样式调整

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -2,5 +2,40 @@
 const app = getApp()
 
 Page({
-
+  data: {
+    composition: [
+      {
+        name: 'energy',
+        cname: '能量',
+      },
+      {
+        name: 'fat',
+        cname: '脂肪',
+      },
+      {
+        name: 'cholesterol',
+        cname: '胆固醇',
+      },
+      {
+        name: 'protein',
+        cname: '蛋白质',
+      },
+      {
+        name: 'carbohydrate',
+        cname: '碳水化合物',
+      },
+    ],
+    foods: [
+      {
+        name: '大黄米（黍）',
+        composition: {
+          energy: 349,
+          protein: 13.6,
+          fat: 2.7,
+          carbohydrate: 67.6,
+          cholesterol: 0,
+        },
+      },
+    ],
+  },
 })
